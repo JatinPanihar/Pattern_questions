5cc8a134fb2f842b0c948d9e0aada2e90c88601f
JATIN <jatin11lakhera@gmail.com>
2024-08-21 22:10:17 +0530

    This is commited file

---
+++ Pattern_question2.java
@@ -0,0 +1,108 @@
+public class Pattern_question2 {
+    public static void main(String[] args) {
+       /*
+       Question 1.**************************************
+       for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+       /*
+        Question 2.****************************************
+        for(int i=0;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+       /*
+       Question 3. *******************************************
+       for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }*/
+        /* Question 4.****************************************
+        for(int k=1;k<=5;k++) {
+             for (int j = 1; j <= k; j++) {
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+         for(int i=1;i<=4;i++){
+             for(int l=4;l>=i;l--){
+                 System.out.print("*");
+             }
+             System.out.println();
+
+         */
+       /* Question 5.******************************************
+       for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+         }*/
+           /*Question 6.**************************************
+            int num = 1;
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=i;j++){
+                   System.out.print(num);
+                   num++;
+                }
+                System.out.println();
+        }
+            */
+        /*Question 7.*****************************************
+       int result = 1;
+       int base = 11;
+       for(int i=4;i>=1;i--){
+       result *= base;
+       System.out.println(result);
+ }*/     ;
+           /*Question 8.*************************************
+            int a = 68;
+            for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                char ch = (char)a;
+                System.out.print(ch);
+            }
+            a--;
+            System.out.println();
+        }*/
+
+       /* Question 9.*******************************************
+            for(int i=1;i<=5;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            for(int l=i;l>=1;l--){
+                System.out.print(l);
+            }
+            System.out.println();
+        }*/
+        /*Question 10.
+            for(int i=1;i<=4;i++){
+            for(int j=3;j>=i;j--) {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            for(int l=2;l<=i;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+         */
+        
+
+    }
+    }
+
