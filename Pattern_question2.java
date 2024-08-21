06e71ab474528ca9abdabe22ed69c063bfc262c5
JATIN <jatin11lakhera@gmail.com>
2024-08-21 22:28:38 +0530

    Changes are done

--- Pattern_question2.java
+++ Pattern_question2.java
@@ -88,7 +88,7 @@ public class Pattern_question2 {
             }
             System.out.println();
         }*/
-        /*Question 10.
+        /*Question 10.******************************************
             for(int i=1;i<=4;i++){
             for(int j=3;j>=i;j--) {
                 System.out.print(" ");
